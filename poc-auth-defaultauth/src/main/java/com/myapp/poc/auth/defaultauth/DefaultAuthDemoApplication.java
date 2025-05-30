59b4b183a513f3fece1515613af023f272f65fca
Harry Xu <harry.xv@gmail.com>
2025-05-30 13:31:00 -0400

    add Spring Security default auth demo module

---
+++ poc-auth-defaultauth/src/main/java/com/myapp/poc/auth/defaultauth/DefaultAuthDemoApplication.java
@@ -0,0 +1,11 @@
+package com.myapp.poc.auth.defaultauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultAuthDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultAuthDemoApplication.class);
+    }
+}
